c5a24c3c05dc6ae3bf7064953647f287cb60cbba
Elijah Verdoorn <elijah.verdoorn@wework.com>
2020-01-19 15:35:47 -0800

    Use short URL links in list

--- app/src/main/java/com/elijahverdoorn/nyt/data/models/Story.kt
+++ app/src/main/java/com/elijahverdoorn/nyt/data/models/Story.kt
@@ -10,5 +10,6 @@ data class Story(
     val abstract: String,
     val byline: String,
     val url: String,
+    val short_url: String?,
     val multimedia: List<Multimedia>? // The API sometimes does not return any multimedia. To handle this case we allow the multimedia value to be nullable.
 )

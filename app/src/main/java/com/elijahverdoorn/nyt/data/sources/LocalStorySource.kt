ba2311ed451ca06dc7088c22f5910fa809118239
Elijah Verdoorn <elijah.verdoorn@wework.com>
2020-01-19 14:52:22 -0800

    Filter all stories on search text change

--- app/src/main/java/com/elijahverdoorn/nyt/data/sources/LocalStorySource.kt
+++ app/src/main/java/com/elijahverdoorn/nyt/data/sources/LocalStorySource.kt
@@ -3,4 +3,6 @@ package com.elijahverdoorn.nyt.data.sources
 import androidx.lifecycle.MutableLiveData
 import com.elijahverdoorn.nyt.data.models.Story
 
-data class LocalStorySource(var stories: MutableLiveData<List<Story>>? = null)
+data class LocalStorySource(
+    var stories: MutableLiveData<List<Story>>? = null,
+    var allStories: List<Story>? = null)
